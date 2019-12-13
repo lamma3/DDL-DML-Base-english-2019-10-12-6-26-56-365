bcdb8ae4c6273d23f6dcb604e6c112fa28bf9282
lamma3 <56866193+lamma3@users.noreply.github.com>
2019-12-13 10:37:35 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+insert into test_table (a) values (1);
 -- Revise record
-
+select * from test_table;
 --  Delete record
-
+delete from test_table where a = 1;
 -- Search record
+select * from test_table where a = 1;
